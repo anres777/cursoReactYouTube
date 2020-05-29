b4efa3c1c5619c2f49f848e680fc2a67f169bfee
Brayan Acosta <bacostacarcamo777@gmail.com>
2020-05-29 02:58:35 -0600

    Uso basico de un componente

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-// const elemento =  document.createElement('h4')
-
-// elemento.innerText="Hola mundo soy Brayan desde un H4"
-
-// const contenedor = document.getElementById("root")
-
-// contenedor.appendChild(elemento)
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// El elemento contiene codigo JSX
-let nombre = "Brayan Acosta"
-const elemento = <h1>Hola mundo {nombre}</h1>
+import Card from './componentes/Card';
 
 // El contenedor es un elemento obtenido de nuestro html
 const contenedor = document.getElementById("root")
 
 //ReactDom recibe dos parametros uno es elemento a renderizar y el otro es el contenedor donde se renderizara
-ReactDOM.render(elemento, contenedor)
+ReactDOM.render(<Card/>, contenedor)
 
 
