b57902831483749d66da0c4a1ca14f1a4b102509
Brayan Acosta <bacostacarcamo777@gmail.com>
2020-06-15 11:30:21 -0600

    Se agregaron estilos en linea y Data JSON

--- src/componentes/Card/Card.js
+++ src/componentes/Card/Card.js
@@ -4,7 +4,8 @@ import React from 'react'
 //Nota: Si deseamos importar estilos CSS debe ir './' si el archivo esta en la misma carpeta
 //que el componente
 import './Card.css'
-
+import circlesImg from '../../images/circles.png'
+import emptyImg from '../../images/empty.png'
 
 
 class Card extends React.Component {
@@ -16,29 +17,33 @@ class Card extends React.Component {
         };        
     }
     
-    componentDidMount(){
-        setTimeout(
-            ()=>{
-                this.setState(
-                    {
-                        image: './../../images/add.png'
-                    }
-                )
-            },
-            5000
-        );
-    }
+    // componentDidMount(){
+    //     setTimeout(
+    //         ()=>{
+    //             this.setState(
+    //                 {
+    //                     image: './../../images/add.png'
+    //                 }
+    //             )
+    //         },
+    //         5000
+    //     );
+    // }
 
     render() {
         //Esta lina crea varias constates con los valors pasados para evitar escribir "this.props" a cada rato
         const { title, description, img, leftColor, rightColor}  = this.props
         console.log(title)
         return (
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card"
+                style={{
+                    backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor || '#56CCF2'}  , ${rightColor|| '#2F80ED'}) `
+                }}
+            >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={this.state.image}/>
+                            <img src={img}/>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
